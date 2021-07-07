0e58d5940b7f05c7f960fdc4a15825c6b64a2e5a
joelGit99 <fernandez.rodriguez.joel.21@gmail.com>
2021-07-07 11:13:40 +0200

    Tarea 3 de la Unidad 4

---
+++ TasquesUD4_3/src/TasquesUD4_3.java
@@ -0,0 +1,34 @@
+
+public class TasquesUD4_3 {
+	/**
+	 * Programa que realiza diferentes operaciones
+	 * @author Joel
+	 */
+	public static void main(String[] args) {
+		int x = 30;
+		int y = 40;
+		double n = 45.3;
+		double m = 12.6;
+		
+		System.out.println("Valor X = " + x);
+		System.out.println("Valor Y = " + y);
+		System.out.println("Valor N = " + n);
+		System.out.println("Valor M = " + m);
+		System.out.println(x + " + " + y + " = " + (x + y));
+		System.out.println(x + " - " + y + " = " + (x - y));
+		System.out.println(x + " x " + y + " = " + (x * y));
+		System.out.println(x + " / " + y + " = " + (x / y));
+		System.out.println(x + " % " + y + " = " + (x % y));
+		System.out.println(x + " + " + n + " = " + (x + n));
+		System.out.println(y + " / " + m + " = " + (y / m));
+		System.out.println(y + " % " + m + " = " + (y % m));
+		System.out.println("El doble de X = " + (x * 2));
+		System.out.println("El doble de Y = " + (y * 2));
+		System.out.println("El doble de N = " + (n * 2));
+		System.out.println("El doble de M = " + (m * 2));
+		System.out.println("La suma de todas las variables es " + (x + y + n + m));
+		System.out.println("El producto de todas las variables es " + (x * y * n * m));
+		
+	}
+
+}
